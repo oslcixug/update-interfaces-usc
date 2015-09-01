aa1e725cf53b6c4ec7aec48b24a70ce06459c5df
Rafael R. Gaioso <rrgaioso@gmail.com>
2015-09-01 13:33:07 +0200

    Primeira versión do script. Non foi probado

---
+++ update-net.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+  echo "Este script debe ser executado por root !!" 2>&1
+  exit 1
+fi
+
+$EDIFICIO=001
+$AULA=001
+
+if cat /proc/net/dev | grep "eth0:" ; then
+
+	INTERFACE="eth0"
+
+else
+
+	INTERFACE="eth1"
+	sed -i -e "s/auto eth0/auto eth1/" /etc/network/interfaces
+	sed -i -e "s/iface etho.*/iface eth1 inet static/" /etc/network/interfaces
+
+fi
+
+PC=$(zenity --entry --text="- EDIFICIO - $EDIFICIO\n- AULA - $AULA\n\nIntroduce o número do equipo:" --title="Oficina de Software Libre")
+
+HOSTNAME=$(zenity --entry --text="Introduce o nome do equipo:" --entry-text=e"$EDIFICIO"a"$AULA"h"$PC"l --title="Oficina de Software Libre")
+
+sed -i -e "s/address.*/address	172.25.9.$PC/" /etc/network/interfaces
+sed -i -e "s/dns -.*/dns-nameservers 192.168.40.12 192.168.40.21/" /etc/network/interfaces
+
+echo $HOSTNAME > /etc/hostname
+sed -i -e "s/127.0.1.1.*/127.0.1.1	$HOSTNAME/" /etc/hosts
+
