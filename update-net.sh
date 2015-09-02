e9ba0054848da47f1477dc2bce4e0162718b6d86
Rafael R. Gaioso <rrgaioso@gmail.com>
2015-09-02 15:07:14 +0200

    #19 - Correción de «etho» por «eth0» #31 - Orde inverso nas ip dos servidores DNS (21 -> 12) #35,#42 - Saída do script ao pulsar o botón Cancelar #11 - Parada do servizo network-manager #46 - Borrado dos ceros á esquerda do número de PC para inserir en IP

--- update-net.sh
+++ update-net.sh
@@ -5,8 +5,14 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-EDIFICIO=001
-AULA=001
+EDIFICIO=170
+VLAN=009
+
+# Para evitar posibles conflictos, paramos o servizo network-manager
+service network-manager stop
+
+# Deshabilitamos a xestión das interfaces de rede a través de Network Manager
+sed -i -e "s/managed=true/managed=false/" /etc/NetworkManager/NetworkManager.conf
 
 if cat /proc/net/dev | grep "eth0:" ; then
 
@@ -16,26 +22,33 @@ else
 
 	INTERFACE="eth1"
 	sed -i -e "s/auto eth0/auto eth1/" /etc/network/interfaces
-	sed -i -e "s/iface etho.*/iface eth1 inet static/" /etc/network/interfaces
+	sed -i -e "s/iface eth0.*/iface eth1 inet static/" /etc/network/interfaces
 
 fi
 
 # Solución para o seguinte erro: zenity: Gtk-WARNING **: cannot open display
 xhost local:$USER
 
-PC=$(zenity --entry --text="- EDIFICIO - $EDIFICIO\n- AULA - $AULA\n\nIntroduce o número do equipo:" --title="Oficina de Software Libre")
+PC=$(zenity --entry --text="- EDIFICIO - $EDIFICIO\n- VLAN - $VLAN\n\nIntroduce o número do equipo:" --title="Oficina de Software Libre")
 
-HOSTNAME=$(zenity --entry --text="Introduce o nome do equipo:" --entry-text=e"$EDIFICIO"a"$AULA"h"$PC"l --title="Oficina de Software Libre")
+# Se o usuario pulsa o botón Cancelar, hai que saír do script
+if [ $? -ne 0 ] ; then
+	exit 1
+fi
 
-sed -i -e "s/address.*/address	172.25.9.$PC/" /etc/network/interfaces
-sed -i -e "s/dns -.*/dns-nameservers 192.168.40.12 192.168.40.21/" /etc/network/interfaces
+HOSTNAME=$(zenity --entry --text="Introduce o nome do equipo:" --entry-text=e"$EDIFICIO"a"$VLAN"h"$PC"l --title="Oficina de Software Libre")
+
+# Se o usuario pulsa o botón Cancelar, hai que saír do script
+if [ $? -ne 0 ] ; then
+	exit 1
+fi
+
+sed -i -e "s/address.*/address	172.25.9.$((10#$PC))/" /etc/network/interfaces
+sed -i -e "s/dns -.*/dns-nameservers 192.168.40.21 192.168.40.12/" /etc/network/interfaces
 sed -i -e "s/gateway.*/gateway	172.25.8.1/" /etc/network/interfaces
 
 echo $HOSTNAME > /etc/hostname
 sed -i -e "s/127.0.1.1.*/127.0.1.1	$HOSTNAME/" /etc/hosts
-
-# Deshabilitamos o Network Manager
-sed -i -e "s/managed=true/managed=false/" /etc/NetworkManager/NetworkManager.conf
 
 zenity --question --text="Completaronse todas as tarefas con éxito.\nQueres reiniciar o equipo?"
 
