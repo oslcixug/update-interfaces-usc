0afc5256e2a246ea50b208cf27b4f8badf15bf60
Rafael R. Gaioso <rrgaioso@gmail.com>
2015-10-13 12:14:51 +0200

    Cambios aportados por Antonio Costa. Uso xenerico do script independente do edificio

--- update-net.sh
+++ update-net.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Version mellorada por Antonio Costa para pedir ao usuario os valores das variables
+# do Edificio e Vlan.
+
 if [[ $EUID -ne 0 ]]; then
   echo "Este script debe ser executado por root !!" 2>&1
   exit 1
 fi
 
 # Variables globais. Deben ser modificadas cos valores indicados para cada aula.
-EDIFICIO=170
-VLAN=009
+# EDIFICIO=170
+# VLAN=009
 
 # Na instalación da aula había un erro nas URL's dos repos de CRAN
 # Modificacion de ficheiro r-cran.list
@@ -34,23 +37,39 @@ fi
 # Solución para o seguinte erro: zenity: Gtk-WARNING **: cannot open display
 xhost local:$USER
 
-PC=$(zenity --entry --text="- EDIFICIO - $EDIFICIO\n- VLAN - $VLAN\n\nIntroduce o número do equipo:" --title="Oficina de Software Libre")
+EDIFICIO=$(zenity --entry --text="Introduce o número do edificio:" --title="Oficina de Software Libre")
+# Se o usuario pulsa o botón Cancelar, hai que saír do script
+if [ $? -ne 0 ] ; then
+exit 1
 
+fi
+VLAN=$(zenity --entry --text="Introduce o número da VLAN:" --title="Oficina de Software Libre")
+# Se o usuario pulsa o botón Cancelar, hai que saír do script
+if [ $? -ne 0 ] ; then
+exit 1
+fi
+
+PC=$(zenity --entry --text="- EDIFICIO - $EDIFICIO\n- VLAN - $VLAN\n\nIntroduce o número do equipo:" --title="Oficina de Software Libre")
 # Se o usuario pulsa o botón Cancelar, hai que saír do script
 if [ $? -ne 0 ] ; then
 	exit 1
 fi
 
 HOSTNAME=$(zenity --entry --text="Introduce o nome do equipo:" --entry-text=e"$EDIFICIO"a"$VLAN"h"$PC"l --title="Oficina de Software Libre")
-
 # Se o usuario pulsa o botón Cancelar, hai que saír do script
 if [ $? -ne 0 ] ; then
 	exit 1
 fi
 
-sed -i -e "s/address.*/address	172.25.9.$((10#$PC))/" /etc/network/interfaces
+ROUTER=$(zenity --entry --text="Introduce a IP do ROUTER:" --title="Oficina de Software Libre")
+# Se o usuario pulsa o botón Cancelar, hai que saír do script
+if [ $? -ne 0 ] ; then
+exit 1
+fi
+
+sed -i -e "s/address.*/address	172.25.$((10#$VLAN))9.$((10#$PC))/" /etc/network/interfaces
 sed -i -e "s/dns -.*/dns-nameservers 192.168.40.21 192.168.40.12/" /etc/network/interfaces
-sed -i -e "s/gateway.*/gateway	172.25.8.1/" /etc/network/interfaces
+sed -i -e "s/gateway.*/gateway	${ROUTER}/" /etc/network/interfaces
 
 # Paramos a interface de rede, e volvemos a levantala coa nova configuración
 ifdown $INTERFACE
